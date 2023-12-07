a3f3b9f640028267f208ac982430f7f58ee418ed
Louis Horrell <lhorrell99@gmail.com>
2023-12-07 17:07:40 +0000

    Day 1 complete

--- day-01/p2.py
+++ day-01/p2.py
@@ -1,18 +1,16 @@
 data_filepath = "day-01/data.txt"
 
-string_digits = [
-    ["one", "1"],
-    ["two", "2"],
-    ["three", "3"],
+string_digit_replacements = [
+    ["one", "o1e"],
+    ["two", "t2o"],
+    ["three", "t3e"],
     ["four", "4"],
-    ["five", "5"],
+    ["five", "5e"],
     ["six", "6"],
-    ["seven", "7"],
-    ["eight", "8"],
-    ["nine", "9"],
+    ["seven", "7n"],
+    ["eight", "e8t"],
+    ["nine", "n9e"],
 ]
-
-string_digits_rev = [[d[0][::-1], d[1]] for d in string_digits]
 
 
 def load_data(filepath):
@@ -21,22 +19,9 @@ def load_data(filepath):
     return data.split("\n")
 
 
-def replace_first_substring(s, match_candidates):
-    positions = []
-
-    for m in match_candidates:
-        pos = s.find(m[0])
-        if pos >= 0:
-            # Substring found in array
-            positions.append(m + [pos])
-
-    if positions:
-        # Return min of positions by third element (position in parent string)
-        first_match = min(positions, key=lambda p: p[2])
-
-        # Replace the first occurence with the corresponding digit
-        s = s.replace(first_match[0], first_match[1], 1)
-
+def replace_substrings(s, candidates):
+    for c in candidates:
+        s = s.replace(c[0], c[1])
     return s
 
 
@@ -49,17 +34,8 @@ def get_first_digit(s):
 # Load data
 puzzle = load_data(data_filepath)
 
-# Replace the first string digit
-puzzle = [replace_first_substring(s, string_digits) for s in puzzle]
-
-# Reverse all the strings
-puzzle = [s[::-1] for s in puzzle]
-
-# Replace all first (reversed) string digits
-puzzle = [replace_first_substring(s, string_digits_rev) for s in puzzle]
-
-# Restore original string order
-puzzle = [s[::-1] for s in puzzle]
+# Replace substrings
+puzzle = [replace_substrings(s, string_digit_replacements) for s in puzzle]
 
 # Extract first and last digits from each string
 puzzle = [get_first_digit(s) + get_first_digit(s[::-1]) for s in puzzle]
