39d5f070899abe02bf80c65655a2e4acf1538887
Louis Horrell <lhorrell99@gmail.com>
2023-12-14 00:21:56 +0000

    Day 10 WIP

---
+++ day-10/p1.py
@@ -0,0 +1,20 @@
+data_filepath = "day-10/data.txt"
+
+
+def load_data(filepath, split_delimiter):
+    """
+    Args:
+        filepath (e.g.): "day-00/data.txt"
+        split_delimiter (e.g.): "\n"
+    Returns:
+        list of entries, split on specified delimiter
+    """
+
+    with open(filepath) as file:
+        data = file.read()
+
+    return data.split(split_delimiter)
+
+
+# Load data
+base_data = load_data(data_filepath, "\n")
