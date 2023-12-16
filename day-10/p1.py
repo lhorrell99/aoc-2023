df3891528b9d3d06bd30efd65d929aeedf37fe5b
Louis Horrell <lhorrell99@gmail.com>
2023-12-16 14:20:03 +0000

    Day 10 part 1 refactor

--- day-10/p1.py
+++ day-10/p1.py
@@ -5,61 +5,12 @@ sys.setrecursionlimit(100000)
 
 data_filepath = "day-10/data.txt"
 
-
-def load_data(filepath, split_delimiter):
-    """
-    Args:
-        filepath (e.g.): "day-00/data.txt"
-        split_delimiter (e.g.): "\n"
-    Returns:
-        list of entries, split on specified delimiter
-    """
-
-    with open(filepath) as file:
-        data = file.read()
-
-    return data.split(split_delimiter)
-
-
-def recurse(a, b, count, map, pipes):
-    """
-    Args:
-        a (e.g.): ("S", 6956)
-        b (e.g.): ("W", 6956)
-        count: 0
-        map: the flattened map
-    Returns:
-        the number of steps to the furthest location from the start point
-    """
-
-    # Base case - indexes aligned and not 1
-    if a[1] == b[1] and count:
-        return count
-
-    # Find new locations in map
-    next_a_loc = a[1] + deltas[a[0]]
-    next_b_loc = b[1] + deltas[b[0]]
-
-    # Find next directions
-    next_a = (pipes[map[next_a_loc]][a[0]], next_a_loc)
-    next_b = (pipes[map[next_b_loc]][b[0]], next_b_loc)
-
-    return recurse(next_a, next_b, count + 1, map, pipes)
-
-
-# Load data
-base_data = load_data(data_filepath, "\n")
-row_len = len(base_data[0])
-
-# Flatten
-map = "".join(base_data)
-
-# Define index deltas for flattened map
+# Define index deltas for each direction
 deltas = {
-    "N": -row_len,
-    "E": 1,
-    "S": row_len,
-    "W": -1,
+    "N": (0, -1),
+    "E": (1, 0),
+    "S": (0, 1),
+    "W": (-1, 0),
 }
 
 # Define from-to pairings for all pipe shapes
@@ -73,9 +24,49 @@ pipes = {
     ".": {},
 }
 
+
+def load_data(filepath, split_delimiter):
+    with open(filepath) as file:
+        data = file.read()
+
+    return data.split(split_delimiter)
+
+
+def get_matching_keys(val, search_dict):
+    keys = [k for k, v in search_dict.items() if v == val]
+    return keys
+
+
+def get_start_dirs(d, start_loc, p_dict, d_dict, map_dict):
+    new_loc = (start_loc[0] + d_dict[d][0], start_loc[1] + d_dict[d][1])
+    return True if d in p_dict[map_dict[new_loc]] else False
+
+
+def recurse(a, b, count, p_dict, d_dict, map_dict):
+    # Base case - indexes aligned (and not at the start)
+    if a[1] == b[1] and count:
+        return count
+
+    # Find new locations in map
+    next_a_loc = (a[1][0] + d_dict[a[0]][0], a[1][1] + d_dict[a[0]][1])
+    next_b_loc = (b[1][0] + d_dict[b[0]][0], b[1][1] + d_dict[b[0]][1])
+
+    # Find next direction pairs
+    next_a = (p_dict[map_dict[next_a_loc]][a[0]], next_a_loc)
+    next_b = (p_dict[map_dict[next_b_loc]][b[0]], next_b_loc)
+
+    return recurse(next_a, next_b, count + 1, p_dict, d_dict, map_dict)
+
+
+# Load data
+base_data = load_data(data_filepath, "\n")
+
+# Build dictionary of indexed values
+map = {(x, y): c for y, r in enumerate(base_data) for x, c in enumerate(r)}
+
 # Find start index and starting directions
-s_loc = map.index("S")
-s_a, s_b = [(d, s_loc) for d in deltas.keys() if d in pipes[map[s_loc + deltas[d]]]]
+loc = get_matching_keys("S", map).pop()
+sa, sb = [[d, loc] for d in deltas.keys() if get_start_dirs(d, loc, pipes, deltas, map)]
 
 # Calculate and display furthest location from start point (result 7066)
-print(recurse(s_a, s_b, 0, map, pipes))
+print(recurse(sa, sb, 0, pipes, deltas, map))
